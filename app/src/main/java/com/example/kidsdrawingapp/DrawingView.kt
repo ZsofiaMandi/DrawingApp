14c3ce1d134a1157c839a020893de88c08d85abc
Zsófia <mandi.zsofia@gmail.com>
2022-02-14 10:42:20 +0100

    Drawing view
    Making a basic drawing view to start with

---
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -0,0 +1,98 @@
+package com.example.kidsdrawingapp
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
+
+    private var mDrawPath : CustomPath? = null
+    private var mCanvasBitmap: Bitmap? = null
+    private var mDrawPaint: Paint? = null
+    private var mCanvasPaint: Paint? = null
+    private var mBrushSize: Float = 0.toFloat()
+    private var color = Color.BLACK
+    private var canvas: Canvas? = null
+    private val mPaths = ArrayList<CustomPath>()
+
+    init{
+        setupDrawing()
+    }
+
+    // Setup the prepared variables
+    private fun setupDrawing(){
+        mDrawPaint = Paint()
+        mDrawPath = CustomPath(color, mBrushSize)
+        mDrawPaint!!.color = color
+        mDrawPaint!!.style = Paint.Style.STROKE
+        mDrawPaint!!.strokeJoin = Paint.Join.ROUND
+        mDrawPaint!!.strokeCap = Paint.Cap.ROUND
+        mCanvasPaint = Paint(Paint.DITHER_FLAG)
+        mBrushSize = 20.toFloat()
+
+    }
+
+    // Setting the canvas size to be correct
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        mCanvasBitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888)
+        canvas = Canvas(mCanvasBitmap!!)
+    }
+
+    // Drawing on Canvas
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.drawBitmap(mCanvasBitmap!!, 0f, 0f, mCanvasPaint)
+
+        // Drawing a path
+        if(!mDrawPath!!.isEmpty){
+            mDrawPaint!!.strokeWidth = mDrawPath!!.brushThickness
+            mDrawPaint!!.color = mDrawPath!!.color
+            canvas.drawPath(mDrawPath!!, mDrawPaint!!)
+        }
+    }
+
+    // Draw on touch -> Give mDrawPath a value, so it wont be empty
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        val touchX = event?.x
+        val touchY = event?.y
+
+        // Motion events we have: finger down on the screen, move and up
+        when(event?.action){
+            MotionEvent.ACTION_DOWN -> {
+                mDrawPath!!.color = color
+                mDrawPath!!.brushThickness = mBrushSize
+
+                mDrawPath!!.reset()
+                if (touchX != null && touchY != null) {
+                    mDrawPath!!.moveTo(touchX, touchY)
+                }
+            }
+
+            MotionEvent.ACTION_MOVE ->{
+                if (touchX != null && touchY != null) {
+                    mDrawPath!!.lineTo(touchX, touchY)
+                }
+            }
+
+            MotionEvent.ACTION_UP ->{
+                mDrawPath = CustomPath(color, mBrushSize)
+            }
+            else -> return false
+        }
+
+        // invalidate the whole view if the view is visible
+        invalidate()
+
+        return true
+    }
+
+
+    internal inner class CustomPath(var color: Int,
+                                    var brushThickness: Float) : Path() {
+
+    }
+
+}
