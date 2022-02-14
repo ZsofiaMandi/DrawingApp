67d657ae9361e7e57bdf3b65bdf94b13f988d8d1
Zsófia <mandi.zsofia@gmail.com>
2022-02-14 16:07:31 +0100

    Brush size selector - part 1
    Creatiing Dialog with the 3 brush sizes

--- app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
+++ app/src/main/java/com/example/kidsdrawingapp/DrawingView.kt
@@ -3,6 +3,7 @@ package com.example.kidsdrawingapp
 import android.content.Context
 import android.graphics.*
 import android.util.AttributeSet
+import android.util.TypedValue
 import android.view.MotionEvent
 import android.view.View
 
@@ -30,7 +31,7 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
         mDrawPaint!!.strokeJoin = Paint.Join.ROUND
         mDrawPaint!!.strokeCap = Paint.Cap.ROUND
         mCanvasPaint = Paint(Paint.DITHER_FLAG)
-        mBrushSize = 20.toFloat()
+        //mBrushSize = 20.toFloat()
 
     }
 
@@ -97,6 +98,14 @@ class DrawingView(context: Context, attrs: AttributeSet) : View(context, attrs){
         invalidate()
 
         return true
+    }
+
+    fun setSizeForBrush(newSize: Float){
+        // Taking the screen size into consideration
+        mBrushSize = TypedValue.applyDimension(
+            TypedValue.COMPLEX_UNIT_DIP,
+            newSize, resources.displayMetrics)
+        mDrawPaint!!.strokeWidth = mBrushSize
     }
 
 
